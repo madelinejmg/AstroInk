32563c44084c270219a0ec14ae36268bb79fb4fd
Madeline Maldonado Gutierrez <124849355+madelinejmg@users.noreply.github.com>
2025-03-28 22:19:58 -0400

    Update summarizer.py

--- src/summarizer.py
+++ src/summarizer.py
@@ -2,5 +2,5 @@ import re
 
 def summarize_text(text, num_sentences = 3):
     sentences = re.split(r'(?<=[.!?]) +', text.strip())
-    summary = sentences[:num_sentences)]
+    summary = sentences[:num_sentences]
     return " ".join(summary)
