23ed585544bb1efb82c3d2638d2d97a6c798fa88
Madeline Maldonado Gutierrez <124849355+madelinejmg@users.noreply.github.com>
2025-03-28 21:15:06 -0400

    Update summarizer.py

--- src/summarizer.py
+++ src/summarizer.py
@@ -1,3 +1,6 @@
+import nltk
+nltk.download('punkt')
+
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
