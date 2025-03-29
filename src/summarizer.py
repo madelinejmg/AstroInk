18788024d819ad7d17da95622825772185768f74
Madeline Maldonado Gutierrez <124849355+madelinejmg@users.noreply.github.com>
2025-03-28 21:17:10 -0400

    Update summarizer.py

--- src/summarizer.py
+++ src/summarizer.py
@@ -1,11 +1,10 @@
-import nltk
-nltk.download('punkt')
-
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
+import nltk
 
 def summarize_text(text, num_sentences=3):
+    nltk.download('punkt')
     parser = PlaintextParser.from_string(text, Tokenizer("english"))
     summarizer = LexRankSummarizer()
     summary = summarizer(parser.document, num_sentences)
