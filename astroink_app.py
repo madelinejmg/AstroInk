f5fa29a2e4e6d1d8002c6a486ad0b0da1682b5a9
Madeline Maldonado Gutierrez <124849355+madelinejmg@users.noreply.github.com>
2025-04-27 21:02:44 -0400

    Update astroink_app.py

--- astroink_app.py
+++ astroink_app.py
@@ -68,7 +68,12 @@ if st.sidebar.button("Search"):
         summary_texts = []
         
         for idx, paper in enumerate(papers):
-            st.markdown(f"### {idx+1}. [{paper['title']}]({paper['url']})")
+            st.markdown(f"### {idx+1}. [{paper['title']}]({paper['url']}) &nbsp; 
+            <span style='background-color: #e0f7fa; 
+            color: #007BFF; padding: 3px 8px; 
+            border-radius: 8px; font-size: 
+            0.8em;'>NEW</span>", unsafe_allow_html=True)
+
 
             # BADGES
             col1, col2, col3 = st.columns(3)
